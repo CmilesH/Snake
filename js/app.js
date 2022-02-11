064fc870f51405e70194d85033ce78e4358d6cdb
CMHend <cmilesh@live.com>
2022-02-11 10:07:36 -0600

    Adjust css. Add clearboard function. Begin render

--- js/app.js
+++ js/app.js
@@ -1,14 +1,11 @@
 /*-------------------------------- Constants --------------------------------*/
 
-const head = snake [0]
 const snakeColor = 'black'
-
-
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-let snake = [  {x: 200, y: 300},  {x: 190, y: 300},  {x: 180, y: 300},  {x: 170, y: 300},  {x: 160, y: 300},]
-
+let snake = [{x: 200, y: 300},  {x: 190, y: 300},  {x: 180, y: 300},  {x: 170, y: 300},  {x: 160, y: 300}]
+let snakeHead = snake [0]
 /*------------------------ Cached Element References ------------------------*/
 
 const gameField = document.getElementById('field')
@@ -20,6 +17,18 @@ const fieldCtx = gameField.getContext('2d')
 
 /*-------------------------------- Functions --------------------------------*/
 
+function render() {
+  setInterval(() => {
+
+  }, 100)
+}
+
+
+function clearBoard() {
+  fieldCtx.fillStyle = 'white'
+  fieldCtx.fillRect(0, 0, gameField.width, gameField.height)
+}
+
 function drawSnakePart(snakePart) {  
   fieldCtx.fillStyle = snakeColor;  
   fieldCtx.fillRect(snakePart.x, snakePart.y, 10, 10);  
@@ -30,13 +39,7 @@ function drawSnake() {
 }
 
 function moveSnake() {
-  head.x + dx
-  head.y + dy
+  newHead = {x: snakeHead.x + dx , y: snakeHead.y + dy}
+  snake.unshift = newHead
+  snake.pop()
 }
-
-
-
-// setTimeout(() => {
-  
-// }, 100
-// drawSnake()
