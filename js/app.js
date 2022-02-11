5fb67924c6381b612f52c37ef24f5dd12571bc16
CMHend <cmilesh@live.com>
2022-02-11 08:26:13 -0600

    Create canvas for drawing snake

--- js/app.js
+++ js/app.js
@@ -1,10 +1,11 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const gameField = document.getElementById('field')
+const fieldCtx = document.getElementById('2d')
 
 
 /*---------------------------- Variables (state) ----------------------------*/
 
-
+let snake = [  {x: 200, y: 200},  {x: 190, y: 200},  {x: 180, y: 200},  {x: 170, y: 200},  {x: 160, y: 200},];
 
 /*------------------------ Cached Element References ------------------------*/
 
@@ -15,3 +16,17 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+
+function drawSnakePart(snakePart) 
+{  
+  snakeboard_ctx.fillStyle = 'lightblue';  
+  snakeboard_ctx.strokestyle = 'darkblue';
+  snakeboard_ctx.fillRect(snakePart.x, snakePart.y, 10, 10);  
+  snakeboard_ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+}
+ 
+/*Function that prints the parts*/
+function drawSnake() 
+{  
+  snake.forEach(drawSnakePart);
+}
