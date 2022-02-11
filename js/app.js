9c9120f90d7c33b1363c895f74fa482a3bbe6fa0
CMHend <cmilesh@live.com>
2022-02-11 10:50:16 -0600

    Add change direction on keypress

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ let snake = [{x: 200, y: 300},  {x: 190, y: 300},  {x: 180, y: 300},  {x: 170, y
 let snakeHead = snake [0]
 let dx = 10
 let dy = 0
+let direction = ''
 /*------------------------ Cached Element References ------------------------*/
 
 const gameField = document.getElementById('field')
@@ -17,14 +18,16 @@ const fieldCtx = gameField.getContext('2d')
 /*----------------------------- Event Listeners -----------------------------*/
 
 keyPress = document.addEventListener('keydown', (e) => {
-  e.code
-  console.log(e.code)
+  direction = e.code
+  console.log(direction)
 })
 
 /*-------------------------------- Functions --------------------------------*/
 
 function render() {
   setInterval(() => {
+    console.log(direction)
+    checkDirection()
     clearBoard()
     drawSnake()
     moveSnake()
@@ -32,6 +35,21 @@ function render() {
   }, 100)
 }
 
+function checkDirection() {
+  if (direction == 'ArrowUp'){
+    dy = -10
+    dx = 0
+  } else if (direction == 'ArrowLeft'){
+    dy = 0
+    dx = -10
+  } else if (direction == 'ArrowDown'){
+    dy = 10
+    dx = 0
+  } else if (direction == 'ArrowRight'){
+    dy = 0
+    dx = 10
+  }
+}
 
 function clearBoard() {
   fieldCtx.fillStyle = 'white'
